797b358132159f3c505be0dee1ce7a9064e0e80f
Hamza <hamza.rahman2016@outlook.com>
2021-06-29 14:01:37 +0500

    Add project files.

---
+++ ConsoleApp1TodoIt/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1TodoIt.Model
+{
+    class Person
+    {
+        int personID;
+        string firstName;
+        string lastName;
+        public Person()
+        {
+
+        }
+    }
+}
